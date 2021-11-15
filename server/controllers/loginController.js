b458257352eaabb20097d6e1c257f573345b9d16
kk7259951 <kk7259951@gmail.com>
2021-11-15 18:01:54 -0500

    Create server, router, controllers to handle log in

---
+++ server/controllers/loginController.js
@@ -0,0 +1,77 @@
+// import DB model
+const db = require('../models/userModel');
+
+const loginController = {};
+
+loginController.getUser = (req, res, next) => {
+  const { username } = req.body;
+  // construct a DB query for a username
+  const query = {
+    text: `
+      SELECT * 
+      FROM users
+      WHERE username = $1;
+    `,
+    params: [username]
+  };
+
+  // query the database
+  db.query(query.text, query.params, (err, dbResponse) => {
+    if (err) {
+      return next({
+        log: 'ERROR: loginController.getUser',
+        message: { err: err.message }
+      });
+    } 
+
+    if (dbResponse) res.locals.user = dbResponse.rows[0];
+    
+    return next();
+  })
+};
+
+loginController.verifyUser = (req, res, next) => {
+  if (!res.locals.user) {
+    return next();
+  }
+
+  const { password } = req.body;
+
+  if (password !== res.locals.user.password) {
+    delete res.locals.user;
+  }
+
+  return next();
+};
+
+loginController.createUser = (req, res, next) => {
+  if (res.locals.user) {
+    delete res.locals.user;
+    return next();
+  }
+
+  const { username, password } = req.body;
+
+  const query = {
+    text: `
+      INSERT INTO users ()
+      VALUES ()
+      RETURNING _id;
+    `,
+    params: [username, password]
+  };
+
+  db.query(query.text, query.params, (err, dbResponse) => {
+    if (err) {
+      return next({
+        log: 'ERROR: loginController.createUser',
+        message: { err: err.message }
+      });
+    }
+
+    res.locals.user = dbResponse.rows[0];
+    return next();
+  });
+};
+
+module.exports = loginController;
