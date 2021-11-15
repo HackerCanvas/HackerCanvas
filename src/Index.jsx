f1c4bcb09ea232a4b921441ce91ca0e9db107266
parker9706 <pdhutcheson@gmail.com>
2021-11-15 13:54:30 -0700

    Adding comment for clarity

--- src/Index.jsx
+++ src/Index.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDom from "react-dom";
 import App from "./App.jsx";
 
+// ReactDom Renders the App Component to our html file, by attaching to the app id
 ReactDom.render(<App />, document.getElementById('app'))
 
